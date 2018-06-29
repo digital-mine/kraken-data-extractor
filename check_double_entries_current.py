0990099e976b74b79c170a9c2db0c51a1cc00dab
DIGITAL MINE <digitalmine23@gmail.com>
2018-06-29 16:25:49 +0100

    Create check_double_entries_current.py

---
+++ check_double_entries_current.py
@@ -0,0 +1,63 @@
+import re
+import urllib 
+from urllib import request
+import time
+from datetime import datetime, timedelta
+import os
+
+start=time.time()
+file=open('trades_.txt','r')
+num=file.readlines()
+file.close()
+allt=[]
+allt_=[]
+z=0
+j=2
+print (len(num))
+for i in num:
+	if z == j:
+		if i not in allt:
+			allt.append(i)
+			allt_.append(num[z-2])
+			allt_.append(num[z-1])
+			allt_.append(i)
+
+		else:
+			if float(i) > 10000:
+				print ('DOUBLE ENTRY:',i)
+
+		j+=3
+	z+=1
+		
+	
+print ('NEW GROUP',len(allt_))	
+
+file=open('new_trades_small.txt', 'w')
+file.close()
+file=open('new_trades_small.txt', 'a')
+for i in allt_:
+	file.write(i)
+
+os.remove('trades_.txt')
+file.close()	
+file=open('new_trades_small.txt','r')
+num=file.readlines()
+file.close()
+
+
+one=(float(num[-3].replace('\n','')))-(float(num[0].replace('\n','')))
+two=one/(float(num[0].replace('\n','')))
+three=two*100
+
+print (PRICE GAIN: three,'%')
+#print ((float(num[0].replace('\n',''))*two)+(float(num[0].replace('\n',''))))
+#print (float(num[-3].replace('\n','')))
+
+
+a=float(num[2].replace('\n',''))
+b= float(num[-1].replace('\n',''))
+delta = b - a
+print ('PERIOD:',(delta/86400),'days')
+print ('PRICE GAIN PER DAY:',(delta/86400)/(three),'% per day')
+os.rename('new_trades_small.txt','trades_.txt')
+print (time.time()-start)
