2991d1ed44091896ac9aad6f1245fba58693eb77
DIGITAL MINE <digitalmine23@gmail.com>
2018-07-23 17:38:59 +0200

    Create launcher.py

---
+++ launcher.py
@@ -0,0 +1,5 @@
+#DO NOT LAUNCH IT AT FIRST USAGE!!
+#PLASE READ THE DOCUMENTATION https://github.com/digital-mine/kraken-data-extractor/wiki
+
+import historical_data
+import check_double_entries_current.py
