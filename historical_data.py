376f4d44821a994170652348398a15f7e9b29508
DIGITAL MINE <digitalmine23@gmail.com>
2018-08-11 13:38:16 +0200

    Update historical_data.py
    Added <buy/sell> data

--- historical_data.py
+++ historical_data.py
@@ -7,13 +7,13 @@ import datetime
 ###
 file=open('last.txt','r')#should create this file before running the program for the first time
 ###
-
 num=file.read()
 if num=='':
 	current=0
 else:
 	num=num.replace('\n','')
-	current=num
+	current=int(num)
+print ('CURRENT',current)
 file.close()
 all=urllib.request.urlopen('https://api.kraken.com/0/public/Trades?pair=XXBTZEUR').read()
 list_all=re.findall("-?\d+\.?\d*",str(all))
@@ -21,35 +21,51 @@ try:
 	last=int(list_all[-1])
 except Exception as e:
 	exit()
-print (last)
 file=open('last.txt','w')
-file.write(last)
+file.write(str(last))
 file.close()
+print ('LAST',last)
 price =[]
 volume =[]
 when= []
+rbs=[]
+s_b_counter=0
 file=open('trades.txt','a')#change this line with the one below after the first run!
 #file=open('trades_.txt','a')
 while last>current:
 	try:
 		all=urllib.request.urlopen('https://api.kraken.com/0/public/Trades?pair=XXBTZEUR&since='+str(current)).read()
 	except Exception as e:
-	        print ('ERRORE LINK')
+	        print ('LINK ERROR')
 	list_all=re.findall("-?\d+\.?\d*",str(all))
+	list_bs=re.findall("[^\W\d_]",str(all))
 	try:
 		current_=list_all[-1]
-		print ('CURRENT',current_)
+		print ('NEXT CURRENT',current_)
 		list_all.remove(list_all[-1])
 		current=int(current_)
 	except Exception as e:
 		print ('ERROR', current_)
 		print (all)
 		current=current
-		print (current,'ERROR CURRENT')
-		print ('Length list trade ERROR', len(list_all))
+		print (current,'CURRENT ERROR')
+		print ('Trade list lenght ERROR', len(list_all))
 	timestamp= float(current/1000000000)
 	value=datetime.datetime.fromtimestamp(timestamp)
 	print (current, value.strftime('%d-%m-%Y %H:%M:%S'))
+	bs=[]
+	for i in list_bs:
+		if i=='s':
+			bs.append(i)
+		if i=='b':
+			bs.append(i)
+	try:
+		bs.remove(bs[0])
+		bs.remove(bs[-1])
+		for i in bs[:-1]:
+			rbs.append(i)
+	except Exception as e:
+		print ('ERROR BS')
 	x=0
 	p=0
 	v=1
@@ -65,15 +81,23 @@ while last>current:
 			v+=3
 		if e==x:
 			file.write(i+'\n')
+			file.write(rbs[s_b_counter]+'\n')
 			when.append(i)
 			e+=3
+			s_b_counter+=1
+			
+			
 		x+=1
 	print ('Length PRICE',len(price))
 	print ('Length VOLUME',len (volume))
 	print ('Length WHEN', len (when))
+	print ('Length BUY SELL', len(rbs))
 	print (price[-1])
 	print (volume[-1])
 	print (when[-1])
+	print (rbs[-1])
 	time.sleep(1)
-  
+print('##################################')
+print ('EXTRACTOR ENDED')
+print('##################################')
 file.close()
